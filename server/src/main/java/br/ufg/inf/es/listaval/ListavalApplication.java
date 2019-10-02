1712ae8f568a325ea7fb8e40181d632641c538fc
Augusto Moura <augustomoura@zgsolucoes.com.br>
2019-10-02 13:32:14 -0300

    primeiro commit server

---
+++ server/src/main/java/br/ufg/inf/es/listaval/ListavalApplication.java
@@ -0,0 +1,13 @@
+package br.ufg.inf.es.listaval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListavalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListavalApplication.class, args);
+	}
+
+}
