11dbd69be454bbc1c5f94c251b9785e818bd101b
Cleydson Júnior <cleydson@zgsolucoes.com.br>
2019-10-23 01:26:40 -0300

    Adiciona controller para crud de AvaliacaoResolucaoLista

---
+++ server/src/main/java/br/ufg/inf/es/listaval/controller/AvaliacaoResolucaoListaController.java
@@ -0,0 +1,45 @@
+package br.ufg.inf.es.listaval.controller;
+
+import br.ufg.inf.es.listaval.AvaliacaoResolucaoListaService;
+import br.ufg.inf.es.listaval.model.aval.AvaliacaoResolucaoLista;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/avaliacoes")
+public class AvaliacaoResolucaoListaController {
+
+	private final AvaliacaoResolucaoListaService avaliacaoResolucaoListaService;
+
+	public AvaliacaoResolucaoListaController(AvaliacaoResolucaoListaService avaliacaoResolucaoListaService) {
+		this.avaliacaoResolucaoListaService = avaliacaoResolucaoListaService;
+	}
+
+	@GetMapping
+	public Page<AvaliacaoResolucaoLista> findAll(Pageable pageable) {
+		return avaliacaoResolucaoListaService.findAll(pageable);
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<AvaliacaoResolucaoLista> findById(@PathVariable("id") Long id) {
+		return avaliacaoResolucaoListaService.findById(id)
+				.map(record -> ResponseEntity.ok().body(record))
+				.orElse(ResponseEntity.notFound().build());
+	}
+
+	@PostMapping
+	public AvaliacaoResolucaoLista create(@Valid @RequestBody AvaliacaoResolucaoLista avaliacaoResolucaoLista) {
+		return avaliacaoResolucaoListaService.save(avaliacaoResolucaoLista);
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<AvaliacaoResolucaoLista> update(@PathVariable("id") Long id, @Valid @RequestBody AvaliacaoResolucaoLista avaliacaoResolucaoLista) {
+		return avaliacaoResolucaoListaService.update(id, avaliacaoResolucaoLista)
+				.map(record -> ResponseEntity.ok().body(record))
+				.orElse(ResponseEntity.notFound().build());
+	}
+}
--- server/src/main/java/br/ufg/inf/es/listaval/repository/aval/AvaliacaoResolucaoListaRepository.java
+++ server/src/main/java/br/ufg/inf/es/listaval/repository/aval/AvaliacaoResolucaoListaRepository.java
@@ -1,7 +1,7 @@
 package br.ufg.inf.es.listaval.repository.aval;
 
-import br.ufg.inf.es.listaval.model.aval.AvaliacaoResposta;
-import org.springframework.data.repository.CrudRepository;
+import br.ufg.inf.es.listaval.model.aval.AvaliacaoResolucaoLista;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface AvaliacaoResolucaoListaRepository extends CrudRepository<AvaliacaoResposta, Long> {
+public interface AvaliacaoResolucaoListaRepository extends PagingAndSortingRepository<AvaliacaoResolucaoLista, Long> {
 }
