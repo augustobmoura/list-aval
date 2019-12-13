f1bce0ac95a4778a7f82893df39f38840136efdf
Augusto Moura <augustomoura@zgsolucoes.com.br>
2019-12-13 18:14:07 -0300

    libera swagger

--- server/src/main/java/br/ufg/inf/es/listaval/config/SecurityConfiguration.java
+++ server/src/main/java/br/ufg/inf/es/listaval/config/SecurityConfiguration.java
@@ -22,6 +22,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Override
 	public void configure(WebSecurity web) {
 		web.ignoring().antMatchers("/api/login");
+		web.ignoring().antMatchers("/v3/api-docs");
 	}
 
 
